c0e6b2d5aede385bd5c21f88bc89d25f7599c271
kumak1 <makino@pepabo.com>
2023-03-29 17:55:19 +0900

    複数リポジトリを指定可能にした

--- main.go
+++ main.go
@@ -16,74 +16,91 @@ import (
 func main() {
 	initArguments()
 
-	repo, err := gh.CurrentRepository()
+	repository, err := gh.CurrentRepository()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// Queryを追加
-	u := &url.URL{}
-	u.Path = fmt.Sprintf("/repos/%s/%s/dependabot/alerts", repo.Owner(), repo.Name())
-	q := u.Query()
-
-	if ecosystem != "" {
-		q.Set("ecosystem", ecosystem)
-	}
-	if scope != "" {
-		q.Set("scope", scope)
-	}
-	if severity != "" {
-		q.Set("severity", severity)
-	}
-	if state != "" {
-		q.Set("state", state)
-	}
-	q.Set("per_page", fmt.Sprint(perPage))
-
-	u.RawQuery = q.Encode()
-
-	if jq == "" {
-		args := []string{"api", "--hostname", repo.Host(), "--jq", ".[] | [.number, .security_advisory.severity, .dependency.package.ecosystem, .dependency.package.name, .html_url, .created_at] | @tsv", u.String()}
-		stdOut, stdErr, err := gh.Exec(args...)
-		if err != nil {
-			log.Fatal(err)
+	var host = repository.Host()
+	var repos = targetRepos(repository.Name())
+	for _, repoName := range repos {
+		if len(repos) > 1 {
+			fmt.Println(repoName)
 		}
-		stdOutString := stdOut.String()
-		stdErrString := stdErr.String()
 
-		if stdOutString != "" {
-			w := new(tabwriter.Writer)
-			w.Init(os.Stdout, 4, 8, 1, '\t', 0)
-			for _, columns := range strings.Split(stdOutString, "\n") {
-				var cols = strings.Split(columns, "\t")
-				if cols[0] == "" {
-					break
-				}
-				cols[0] = formatIndex(cols[0])
-				cols[1] = formatSeverity(cols[1])
-				cols[5] = formatDate(cols[5])
-				_, _ = fmt.Fprintln(w, strings.Join(cols, "\t"))
+		u := &url.URL{}
+		u.Path = fmt.Sprintf("/repos/%s/%s/dependabot/alerts", repository.Owner(), repoName)
+		q := u.Query()
+
+		if ecosystem != "" {
+			q.Set("ecosystem", ecosystem)
+		}
+		if scope != "" {
+			q.Set("scope", scope)
+		}
+		if severity != "" {
+			q.Set("severity", severity)
+		}
+		if state != "" {
+			q.Set("state", state)
+		}
+		q.Set("per_page", fmt.Sprint(perPage))
+
+		u.RawQuery = q.Encode()
+
+		if jq == "" {
+			args := []string{"api", "--hostname", host, "--jq", ".[] | [.number, .security_advisory.severity, .dependency.package.ecosystem, .dependency.package.name, .html_url, .created_at] | @tsv", u.String()}
+			stdOut, stdErr, err := gh.Exec(args...)
+			if err != nil {
+				fmt.Println("error")
+				log.Fatal(err)
 			}
-			_ = w.Flush()
-		}
-		if stdErrString != "" {
-			fmt.Print(stdErrString)
-		}
-	} else {
-		args := []string{"api", "--hostname", repo.Host(), "--jq", jq, u.String()}
-		stdOut, stdErr, err := gh.Exec(args...)
-		if err != nil {
-			log.Fatal(err)
-		}
-		stdOutString := stdOut.String()
-		stdErrString := stdErr.String()
+			stdOutString := stdOut.String()
+			stdErrString := stdErr.String()
 
-		if stdOutString != "" {
-			fmt.Print(stdOutString)
+			if stdOutString != "" {
+				w := new(tabwriter.Writer)
+				w.Init(os.Stdout, 4, 8, 1, '\t', 0)
+				for _, columns := range strings.Split(stdOutString, "\n") {
+					var cols = strings.Split(columns, "\t")
+					if cols[0] == "" {
+						break
+					}
+					cols[0] = formatIndex(cols[0])
+					cols[1] = formatSeverity(cols[1])
+					cols[5] = formatDate(cols[5])
+					_, _ = fmt.Fprintln(w, strings.Join(cols, "\t"))
+				}
+				_ = w.Flush()
+			}
+			if stdErrString != "" {
+				fmt.Print("stdErrString")
+				fmt.Print(stdErrString)
+			}
+		} else {
+			args := []string{"api", "--hostname", host, "--jq", jq, u.String()}
+			stdOut, stdErr, err := gh.Exec(args...)
+			if err != nil {
+				log.Fatal(err)
+			}
+			stdOutString := stdOut.String()
+			stdErrString := stdErr.String()
+
+			if stdOutString != "" {
+				fmt.Print(stdOutString)
+			}
+			if stdErrString != "" {
+				fmt.Print(stdErrString)
+			}
 		}
-		if stdErrString != "" {
-			fmt.Print(stdErrString)
-		}
+	}
+}
+
+func targetRepos(defaultRepoName string) []string {
+	if len(repositories) == 0 {
+		return []string{defaultRepoName}
+	} else {
+		return repositories
 	}
 }
 
@@ -111,14 +128,18 @@ func formatSeverity(severity string) string {
 	}
 }
 
-var ecosystem string
-var scope string
-var severity string
-var state string
-var perPage int
-var jq string
+var (
+	repositories []string
+	ecosystem    string
+	scope        string
+	severity     string
+	state        string
+	perPage      int
+	jq           string
+)
 
 func initArguments() {
+	flags.StringArrayVarP(&repositories, "repo", "r", []string{}, "")
 	flags.StringVarP(&ecosystem, "ecosystem", "e", "", "specify comma-separated list. can be: composer, go, maven, npm, nuget, pip, pub, rubygems, rust")
 	flags.StringVar(&scope, "scope", "", "specify comma-separated list. can be: development, runtime")
 	flags.StringVar(&severity, "severity", "", "specify comma-separated list. can be: low, medium, high, critical")
